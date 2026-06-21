40236a55cd6c9e80583f284e12a7fba041462f3f
Júlio César França Rodrigues <jcking0@hotmail.com>
2026-06-20 21:26:27 -0300

    Removendo bloco migrations do prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,9 +6,6 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
     url: env("DATABASE_URL"),
   },
